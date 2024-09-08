114163dd8029628a96e4dd148f7bf1de36c78599
srvrshard1 <srvrshard1@gmail.com>
2024-09-08 19:43:50 -0400

    myCommitMsg

---
+++ manifestData.js
@@ -0,0 +1,10 @@
+const manifest = {
+  commitFile: "./manifestData.js",
+    commitMsg: "myCommitMsg",
+    commitTag: "20240208",
+    commitApp: "App_Portfolio",
+    commitData: {
+      "dataKeyBin": "dataValueBin",
+    },
+}
+module.exports = manifest;
