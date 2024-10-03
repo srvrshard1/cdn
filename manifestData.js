bff779609e09aa93ae3bcaa6cb7029165a774ad6
srvrshard1 <srvrshard1@gmail.com>
2024-10-03 00:22:15 -0400

    myCommitMsg2

--- manifestData.js
+++ manifestData.js
@@ -1,7 +1,7 @@
 const manifestData = {
   commitFile: "./manifestData.js",
     commitMsg: "myCommitMsg2",
-    commitTag: "58dce479ae",
+    commitTag: "173a7b4a06",
     commitApp: "App_App",
     commitData: {
       "dataKeyBin2": "dataValueBin2",
